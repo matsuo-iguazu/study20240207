4c1d781f8c17a432c7014292bcc1b0c08d367ef5
matsuo-iguazu <83324692+matsuo-iguazu@users.noreply.github.com>
2024-01-24 13:56:13 +0900

    change listen port to 8080

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
+// const port = 3001
+const port = 8080
 
 app.get('/', (req, res) => {console.log("Get request is called")
   res.send('Hello World! Yo_oY')
