1e1fe49f749bc581b374316db6125d112cfe22a2
matsuo-iguazu <83324692+matsuo-iguazu@users.noreply.github.com>
2024-02-07 20:00:26 +0900

    Add Assistant Embed code

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express')
-const app = express()
-// const port = 3001
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {console.log("Get request is called")
-  res.send('Hello World! Yo_oY')
-})
+// Watson AssistantのEmbedコード
+const watsonAssistantScript = `
+  <script>
+    window.watsonAssistantChatOptions = {
+      integrationID: "5b5cbd62-badc-4082-b10a-136ddfc5efac",
+      region: "us-south",
+      serviceInstanceID: "38b61b33-64db-4bb2-8f74-87cc16145ee6",
+      onLoad: async (instance) => { await instance.render(); }
+    };
+    setTimeout(function(){
+      const t=document.createElement('script');
+      t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+      document.head.appendChild(t);
+    });
+  </script>
+`;
 
+// ルートパスのエンドポイント
+app.get('/', (req, res) => {
+  res.send(watsonAssistantScript);
+});
+
+// Expressアプリケーションを起動
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
